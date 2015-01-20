b36c1ce536c65116fa91e495ba0f73524510a117
toshi <toshi@toshiaki-ishibashi-no-MacBook-Pro-2.local>
2015-01-20 15:46:01 +0900

    a

--- sample.js
+++ sample.js
@@ -2,6 +2,6 @@
   window.setInterval(function(){
     var totalViewable = $sf.ext.inViewPercentage();
     var elem = document.getElementById("inview");
-    elem.innerHTML = "Viewable: " + totalViewable + "%";
+    document.write("Viewable: " + totalViewable + "%");
   }, 50);
 })();
