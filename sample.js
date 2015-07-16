77c897edff91645fcfeaa44aa35a5061ea9f45bb
Toshiaki Ishibashi <toshi.ishibashi@gmail.com>
2015-07-16 10:22:24 +0900

    aaa

--- sample.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  window.setInterval(function(){
-    var totalViewable = $sf.ext.inViewPercentage();
-    var elem = document.getElementById("inview");
-    document.write("Viewable: " + totalViewable + "%");
-  }, 50);
-})();
