aac3a8753950b8c050c839f6bb524f652a789392
Toshi Ishibashi <toshiaki.ishibashi@rakuten.com>
2020-07-31 18:08:34 +0900

    aaa

---
+++ script.js
@@ -0,0 +1,6 @@
+var str = '\
+<script src="https://z.moatads.com/rakutenjpgendisplay377348550985/moatad.js#moatClientLevel1={ADVERTISER_ID}&moatClientLevel2={ORDER_ID}&moatClientLevel3={LINE_ITEM_ID}&moatClientLevel4={CREATIVE_ID}" type="text/javascript"></script>';
+document.write(str);
+
+
+
